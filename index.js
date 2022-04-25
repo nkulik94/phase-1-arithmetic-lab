187b3c0af6f707b3b5bf8bc829efca9a8b2884d1
Naftali Kulik <nkulik1994@gmail.com>
2022-04-25 16:41:00 -0400

    commit tree 2fd6fb1e9029a4d46595218bdfc962fb84d2fe57

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1
+const num3 = 19
+const num4 = 5
+const mod = num3 % num4
+const max = Math.max(1, 2, 3, 20)
